86c4a98165f55cf5d7005317725e12fc789e73d3
nyawera <nyaweratutchuol@gmail.com>
2022-02-13 19:12:40 +0300

    firstkotlin project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val name="Nyawera Tut"
+    println(name)
+    val age:Byte= 21
+    println(age)
+    val phoneNumber:String="254706451669"
+    println(phoneNumber)
+    val weight=52
+    println(weight)
+    val studentkenyancitizen:Boolean=true
+    println(studentkenyancitizen)
+    val studentnotkenyancitizen:Boolean=false
+    println(studentnotkenyancitizen)
+
+}
